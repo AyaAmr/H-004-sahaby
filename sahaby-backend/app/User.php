b2fb4e983a6d5866ad130dfc1fde734f0c188b52
Aya Amr <aya.amr.mostafa@gmail.com>
2018-08-02 06:17:36 +0300

    update user model

--- sahaby-backend/app/User.php
+++ sahaby-backend/app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'phone_number', 'password', 'fb_id',
     ];
 
     /**
@@ -26,4 +26,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
 }
